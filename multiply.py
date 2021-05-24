bfe1fe84115d5238953c7003bed70203304b219f
student <audreymunyiva@gmail.com>
2021-05-24 12:57:45 +0300

    first commit

---
+++ multiply.py
@@ -0,0 +1,10 @@
+print(" \t \t Maths is fun ")
+
+for i in range (1,100):
+    print(i ,end="\t")
+for j in range (1,100):
+    for k in range (1,100):
+          print(j * k , end="\t")
+    print("\n")
+
+    
